a8f9d47b29bd552c1fbc351482044d03ae7470a1
James Fox-Collis <jlfoxcollis@gmail.com>
2020-10-13 09:30:23 -0700

    Updated merge test name

--- test/merge_test.rb
+++ test/merge_test.rb
@@ -4,7 +4,7 @@ require "./lib/merge_error"
 require "pry";
 
 class Test < Minitest::Test
-  def test_it_exists
+  def test_it_exists_because_it_does
     merge = Merge.new
     assert_instance_of Merge, merge
   end
